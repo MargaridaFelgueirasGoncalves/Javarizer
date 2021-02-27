3ee88d3d03cdd9299b12ea5a383b006cc19de06b
vniacsta <vaniarcosta26@gmail.com>
2021-02-27 15:49:27 +0000

    new class to display menu picture

---
+++ src/org/academiadecodigo/weekendteamwork/javarizer/server/ServerPic.java
@@ -0,0 +1,12 @@
+package org.academiadecodigo.weekendteamwork.javarizer.server;
+
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+public class ServerPic {
+
+    /**
+     * fields
+     */
+//    private Rectangle
+}
