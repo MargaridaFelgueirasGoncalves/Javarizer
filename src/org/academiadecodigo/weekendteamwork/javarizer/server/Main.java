009e304774be26f9003cf4b2ca5cdcedad50684e
vniacsta <vaniarcosta26@gmail.com>
2021-02-27 20:59:13 +0000

    updated quiz.txt and menu with simple graphics

--- src/org/academiadecodigo/weekendteamwork/javarizer/server/Main.java
+++ src/org/academiadecodigo/weekendteamwork/javarizer/server/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        new ServerPic();
         Server server = new Server(PORT);
         server.start();
     }
--- src/org/academiadecodigo/weekendteamwork/javarizer/server/ServerPic.java
+++ src/org/academiadecodigo/weekendteamwork/javarizer/server/ServerPic.java
@@ -8,5 +8,22 @@ public class ServerPic {
     /**
      * fields
      */
-//    private Rectangle
+    private Rectangle canvas;
+    private final int PADDING = 10;
+
+    /**
+     * constructor
+     */
+    public ServerPic() {
+
+        canvas = new Rectangle(PADDING, PADDING, 610, 400);
+        canvas.fill();
+        addPicture();
+    }
+
+    public void addPicture() {
+
+        Picture javarizer = new Picture(10, 10, "resources/javarizer.png");
+        javarizer.draw();
+    }
 }
